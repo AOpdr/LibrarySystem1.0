c2b489bdd25413586327cc73ef85be9ad5ea7052
青木 ペドロ <p.aoki@fmsystem.co.jp>
2025-08-04 18:44:53 +0900

    feat(books): 書籍一覧ページを実装
    書籍一覧を（仮のデータで）表示する機能を追加。
    データベース接続の前に、MVCの基本構造を確立する。
    
    - `Book.cs` モデルを作成
    - `BooksController` を追加し、ハードコードした書籍リストを準備
    - `Index.cshtml` ビューを作成し、テーブルで書籍一覧を表示
    
    ## 反省点
    Controllerのファイル名をタイポ（BookController）してしまい、404エラーの解決に少し時間がかかった。

---
+++ LibrarySystem1.0/LibrarySystem1.0/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LibrarySystem1._0.Models; // ←先ほど作ったModels/Book.csの情報を使う
+
+namespace LibrarySystem1._0.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            // DBに繋げる前にデータを表示させるため、適当なデータを作っておく
+            var books = new List<Book>
+            {
+                new Book { BookId = 1, Title = "Peter's book", Author = "Aoki Pedro" },
+                new Book { BookId = 2, Title = "今夜もカップラーメン", Author = "青木ぺど郎" },
+                new Book { BookId = 2, Title = "明日天気になあれ", Author = "やすお" }
+            };
+
+            // Viewに上で作ったbooksのデータ（書籍リスト）を渡す
+            return View(books);
+        }
+    }
+}
---
+++ LibrarySystem1.0/LibrarySystem1.0/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibrarySystem1._0.Models // ←今実際に作っているプロジェクト名
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
